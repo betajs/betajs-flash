f41d1fba4d26aaf3ed486f8d79d7e8fdb2c55fad
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-12-23 12:00:20 -0500

    Typo

--- vendors/beta-noscoped.js
+++ vendors/beta-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs - v1.0.22 - 2015-12-12
+betajs - v1.0.25 - 2015-12-23
 Copyright (c) Oliver Friedmann,Victor Lingenthal
 MIT Software License.
 */
@@ -12,7 +12,7 @@ Scoped.binding("module", "global:BetaJS");
 Scoped.define("module:", function () {
 	return {
 		guid: "71366f7a-7da3-4e55-9a0b-ea0e4e2a9e79",
-		version: '444.1449951728394'
+		version: '449.1450889499906'
 	};
 });
 
@@ -515,7 +515,7 @@ Scoped.define("module:Class", ["module:Types", "module:Objs", "module:Functions"
 		if (this.__auto_destroy_list) {
 			for (var i = 0; i < this.__auto_destroy_list.length; ++i) {
 				if ("destroy" in this.__auto_destroy_list[i])
-					this.__auto_destroy_list[i].destroy();
+					this.__auto_destroy_list[i].weakDestroy();
 			}
 		}
 		var cid = this.cid();
@@ -575,6 +575,39 @@ Scoped.define("module:Class", ["module:Types", "module:Objs", "module:Functions"
 		return this.cls.ancestor_of(cls);
 	};
 	
+	Class.prototype.inspect = function () {
+		return {
+			header: {
+				cid: this.cid(),
+				classname: this.cls.classname,
+				destroyed: this.destroyed()
+			},
+			attributes: {
+				attributes_public: Objs.filter(this, function (value, key) {
+					return !Types.is_function(value) && key.indexOf("_") !== 0;
+				}, this),
+				attributes_protected: Objs.filter(this, function (value, key) {
+					return !Types.is_function(value) && key.indexOf("_") === 0 && key.indexOf("__") !== 0;
+				}, this),
+				attributes_private: Objs.filter(this, function (value, key) {
+					return !Types.is_function(value) && key.indexOf("__") === 0;
+				}, this)
+			},
+			methods: {
+				methods_public: Objs.filter(this, function (value, key) {
+					return Types.is_function(value) && key.indexOf("_") !== 0;
+				}, this),
+				methods_protected: Objs.filter(this, function (value, key) {
+					return Types.is_function(value) && key.indexOf("_") === 0 && key.indexOf("__") !== 0;
+				}, this),
+				method_private: Objs.filter(this, function (value, key) {
+					return Types.is_function(value) && key.indexOf("__") === 0;
+				}, this)
+			}
+		};
+	};
+
+	
 	// Legacy Methods
 	
 	Class.prototype._auto_destroy = function(obj) {
@@ -584,7 +617,7 @@ Scoped.define("module:Class", ["module:Types", "module:Objs", "module:Functions"
 	Class.prototype._inherited = function (cls, func) {
 		return cls.parent.prototype[func].apply(this, Array.prototype.slice.apply(arguments, [2]));
 	};
-		
+	
 	return Class;
 
 });
@@ -1551,7 +1584,13 @@ Scoped.define("module:Events.EventsMixin", [
 				if (!this.__events_mixin_events[event])
 					this._notify("register_event", event);
 				this.__events_mixin_events[event] = this.__events_mixin_events[event] || new LinkedList();
-				this.__events_mixin_events[event].add(this.__create_event_object(callback, context, options));
+				var event_object = this.__create_event_object(callback, context, options);
+				this.__events_mixin_events[event].add(event_object);
+				if (this.__events_mixin_persistent_events && this.__events_mixin_persistent_events[event]) {
+					var argss = this.__events_mixin_persistent_events[event];
+					for (var i = 0; i < argss.length; ++i)
+						this.__call_event_object(event_object, argss[i]);
+				}
 			}
 			return this;
 		},
@@ -1624,6 +1663,17 @@ Scoped.define("module:Events.EventsMixin", [
 					});
 			}, this);
 			return this;
+		},
+		
+		persistentTrigger: function (events) {
+			this.trigger.apply(this, arguments);
+			events = events.split(this.EVENT_SPLITTER);
+			var rest = Functions.getArguments(arguments, 1);
+			this.__events_mixin_persistent_events = this.__events_mixin_persistent_events || [];
+			Objs.iter(events, function (event) {
+				this.__events_mixin_persistent_events[event] = this.__events_mixin_persistent_events[event] || [];
+				this.__events_mixin_persistent_events[event].push(rest);
+			}, this);
 		},
 
 		once: function (events, callback, context, options) {
@@ -3454,8 +3504,13 @@ Scoped.define("module:Parser.Lexer", ["module:Class", "module:Types", "module:Ob
 });
 
 
-Scoped.define("module:Promise", ["module:Types", "module:Functions", "module:Async"], function (Types, Functions, Async) {
-	return {		
+Scoped.define("module:Promise", [
+    "module:Types",
+    "module:Functions",
+    "module:Async",
+    "module:Objs"
+], function (Types, Functions, Async, Objs) {
+	var Promise = {		
 			
 		Promise: function (value, error, finished) {
 			this.__value = error ? null : (value || null);
@@ -3626,13 +3681,8 @@ Scoped.define("module:Promise", ["module:Types", "module:Functions", "module:Asy
 		}
 		
 	};
-});
-
-
-Scoped.extend("module:Promise.Promise.prototype", ["module:Promise", "module:Functions"], function (Promise, Functions) {
 	
-	return {
-		
+	Objs.extend(Promise.Promise.prototype, {
 		classGuid: "7e3ed52f-22da-4e9c-95a4-e9bb877a3935",
 		
 		success: function (f, context, options) {
@@ -3786,10 +3836,12 @@ Scoped.extend("module:Promise.Promise.prototype", ["module:Promise", "module:Fun
 			var result = Promise.and(this);
 			return result.and(promises);
 		}
-			
-	};
+	});
 	
+	return Promise;
 });
+
+
 Scoped.define("module:Properties.PropertiesMixin", [
     "module:Objs.Scopes",
     "module:Objs",
@@ -4005,7 +4057,7 @@ Scoped.define("module:Properties.PropertiesMixin", [
 				});
 				self.set(key, func.apply(args.context, values));
 			}
-			BetaJS.Objs.iter(deps, function (dep) {
+			Objs.iter(deps, function (dep) {
 				var value = dep.properties.get(dep.key);
 				// Ugly way of checking whether an EventsMixin is present - please improve in the future on this
 				if (value && typeof value == "object" && "on" in value && "off" in value && "trigger" in value) {
@@ -4663,7 +4715,7 @@ Scoped.define("module:Router.RouteParser", [ "module:Class", "module:Strings",
 
 			constructor : function(routes) {
 				inherited.constructor.call(this);
-				this.routes = [];
+				this.routes = {};
 				Objs.iter(routes, function(route, key) {
 					this.bind(key, route);
 				}, this);
@@ -4865,11 +4917,14 @@ Scoped.define("module:Router.StateRouteBinder", [ "module:Router.RouteBinder", "
 
 			constructor : function(router, stateHost, options) {
 				this._stateHost = stateHost;
-				this._options = options || {};
+				options = Objs.extend({
+					capitalizeStates: false
+				}, options);
+				this._options = options;
 				this._routeToState = new RouteMap({
 					map : this._options.routeToState || function (name, args) {
 						return {
-							name: Strings.capitalize(name),
+							name: options.capitalizeStates ? Strings.capitalize(name) : name,
 							args: args
 						};
 					},
@@ -4907,7 +4962,7 @@ Scoped.define("module:Router.StateRouteBinder", [ "module:Router.RouteBinder", "
 
 			register: function (name, route, extension) {
 				this._router.bind(name, route);
-				this._stateHost.register(Strings.capitalize(name), extension);
+				this._stateHost.register(this._options.capitalizeStates ? Strings.capitalize(name) : name, extension);
 				return this;
 			},			
 
--- vendors/betajs-browser-noscoped.js
+++ vendors/betajs-browser-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs-browser - v1.0.12 - 2015-12-12
+betajs-browser - v1.0.17 - 2015-12-23
 Copyright (c) Oliver Friedmann
 MIT Software License.
 */
@@ -21,7 +21,7 @@ Scoped.define("base:$", ["jquery:"], function (jquery) {
 Scoped.define("module:", function () {
 	return {
 		guid: "02450b15-9bbf-4be2-b8f6-b483bc015d06",
-		version: '58.1449951831697'
+		version: '63.1450889613503'
 	};
 });
 
@@ -509,7 +509,7 @@ Scoped.define("module:Dom", [
 				return s;
 			var temp = document.createElement("span");
 			temp.innerHTML = s;
-			s = temp.innerText;
+			s = $(temp).text();
 			if (temp.remove)
 				temp.remove();
 			return s;
@@ -518,8 +518,14 @@ Scoped.define("module:Dom", [
 	};
 });
 Scoped.define("module:DomExtend.DomExtension", [
-    "base:Class", "jquery:", "base:Objs", "base:Functions", "base:Async"                                                   
-], function (Class, jquery, Objs, Functions, Async, scoped) {
+    "base:Class",
+    "jquery:",
+    "base:Objs",
+    "base:Functions",
+    "base:Async",
+    "module:DomMutation.NodeRemoveObserver",
+    "module:DomMutation.NodeResizeObserver"
+], function (Class, jquery, Objs, Functions, Async, NodeRemoveObserver, NodeResizeObserver, scoped) {
 	return Class.extend({scoped: scoped}, function (inherited) {
 		return {
 			
@@ -541,25 +547,16 @@ Scoped.define("module:DomExtend.DomExtension", [
 					this._element[method] = Functions.as_method(this[method], this);
 				}, this);
 				Async.eventually(function () {
-					var self = this;
-					$(document).on("DOMNodeRemoved." + this.cid(), function (event) {
-						if (event.target === element) {
-							self.weakDestroy();
-						}
-					});
-					$(window).on("resize." + this.cid(), function () {
-						self.recomputeBB();
-						self._notify("resized");
-					});
+					this._nodeRemoveObserver = this.auto_destroy(new NodeRemoveObserver(element));
+					this._nodeRemoveObserver.on("node-removed", this.weakDestroy, this);
+					this._nodeResizeObserver = this.auto_destroy(new NodeResizeObserver(element));
+					this._nodeResizeObserver.on("node-resized", function () {
+						this.recomputeBB();
+						this._notify("resized");
+					}, this);
 				}, this);
 				if (!this._$element.css("display") || this._$element.css("display") == "inline")
 					this._$element.css("display", "inline-block");
-			},
-			
-			destroy: function () {
-				$(window).off("." + this.cid());
-				$(document).off("." + this.cid());
-				inherited.destroy.call(this);
 			},
 			
 			domEvent: function (eventName) {
@@ -607,10 +604,15 @@ Scoped.define("module:DomExtend.DomExtension", [
 			
 			computeActualBB: function (idealBB) {
 				var width = this._$element.width();
-				if (this._$element.width() < idealBB.width && !this._element.style.width) {
+				if (this._$element.width() <= idealBB.width && !this._element.style.width) {
 					this._element.style.width = idealBB.width + "px";
 					width = this._$element.width();
-					delete this._element.style.width;
+					var current = this._$element;
+					while (current.get(0) != document) {
+						current = current.parent();
+						width = Math.min(width, current.width());
+					}
+					this._element.style.width = null;
 				}
 				return {
 					width: width,
@@ -636,6 +638,249 @@ Scoped.define("module:DomExtend.DomExtension", [
 			
 		};
 	});
+});
+
+Scoped.define("module:DomMutation.NodeRemoveObserver", [
+    "base:Classes.ConditionalInstance",
+    "base:Events.EventsMixin"
+], function (ConditionalInstance, EventsMixin, scoped) {
+	return ConditionalInstance.extend({scoped: scoped}, [EventsMixin, function (inherited) {
+		return {
+			
+			constructor: function (node) {
+				inherited.constructor.call(this);
+				this._node = node;
+			},
+			
+			_nodeRemoved: function (node) {
+				if (node !== this._node)
+					return;
+				this.trigger("node-removed");
+			}
+			
+		};
+	}]);
+});
+
+
+
+Scoped.define("module:DomMutation.MutationObserverNodeRemoveObserver", [
+	"module:DomMutation.NodeRemoveObserver",
+	"base:Objs"
+], function (Observer, Objs, scoped) {
+	return Observer.extend({scoped: scoped}, function (inherited) {
+		return {
+			
+			constructor: function (node) {
+				inherited.constructor.call(this, node);
+				var self = this;
+				this._observer = new MutationObserver(function (mutations) {
+					Objs.iter(mutations, function (mutation) {
+						for (var i = 0; i < mutation.removedNodes.length; ++i)
+							self._nodeRemoved(mutation.removedNodes[i]);
+					});
+				});
+				this._observer.observe(node.parentNode, {childList: true});
+			},
+			
+			destroy: function () {
+				this._observer.disconnect();
+				inherited.destroy.call(this);
+			}
+			
+		};
+	}, {
+		
+		supported: function (node) {
+			try {
+				return !!window.MutationObserver;
+			} catch (e) {
+				return false;
+			}
+		}
+		
+	});	
+});
+
+
+
+Scoped.define("module:DomMutation.DOMNodeRemovedNodeRemoveObserver", [
+	"module:DomMutation.NodeRemoveObserver",
+	"jquery:"
+], function (Observer, $, scoped) {
+	return Observer.extend({scoped: scoped}, function (inherited) {
+		return {
+			
+			constructor: function (node) {
+				inherited.constructor.call(this, node);
+				var self = this;
+				$(document).on("DOMNodeRemoved." + this.cid(), function (event) {
+					self._nodeRemoved(event.target);
+				});
+			},
+			
+			destroy: function () {
+				$(document).off("DOMNodeRemoved." + this.cid());
+				inherited.destroy.call(this);
+			}
+			
+		};
+	}, {
+		
+		supported: function (node) {
+			return true;
+		}
+		
+	});	
+
+});
+
+
+Scoped.extend("module:DomMutation.NodeRemoveObserver", [
+    "module:DomMutation.NodeRemoveObserver",
+    "module:DomMutation.MutationObserverNodeRemoveObserver",
+    "module:DomMutation.DOMNodeRemovedNodeRemoveObserver"
+], function (Observer, MutationObserverNodeRemoveObserver, DOMNodeRemovedNodeRemoveObserver) {
+	Observer.register(MutationObserverNodeRemoveObserver, 2);
+	Observer.register(DOMNodeRemovedNodeRemoveObserver, 1);
+	return {};
+});
+
+
+Scoped.define("module:DomMutation.NodeResizeObserver", [
+    "base:Class",
+    "base:Events.EventsMixin",
+    "jquery:"
+], function (Class, EventsMixin, $, scoped) {
+	return Class.extend({scoped: scoped}, [EventsMixin, function (inherited) {
+		return {
+			
+			constructor: function (node) {
+				inherited.constructor.call(this);
+				var self = this;
+				$(window).on("resize." + this.cid(), function () {
+					self._resized();
+				});
+			},
+			
+			destroy: function () {
+				$(window).off("." + this.cid());
+				inherited.destroy.call(this);
+			},
+			
+			_resized: function () {
+				this.trigger("node-resized");
+			}
+			
+		};
+	}]);
+});
+
+
+Scoped.define("module:DomMutation.NodeInsertObserver", [
+	"base:Classes.ConditionalInstance",
+	"base:Events.EventsMixin"
+], function (ConditionalInstance, EventsMixin, scoped) {
+	return ConditionalInstance.extend({scoped: scoped}, [EventsMixin, function (inherited) {
+		return {
+			
+			_nodeInserted: function (node) {
+				if (this._options.parent && node.parentNode !== this._options.parent)
+					return;
+				if (this._options.root && !this._options.root.contains(node))
+					return;
+				if (this._options.filter && !this._options.filter.call(this._options.context || this, node))
+					return;
+				this.trigger("node-inserted", node);
+			}
+			
+		};
+	}]);
+});
+
+
+Scoped.define("module:DomMutation.MutationObserverNodeInsertObserver", [
+	"module:DomMutation.NodeInsertObserver",
+	"base:Objs"
+], function (Observer, Objs, scoped) {
+	return Observer.extend({scoped: scoped}, function (inherited) {
+		return {
+			
+			constructor: function (options) {
+				inherited.constructor.call(this, options);
+				var self = this;
+				this._observer = new MutationObserver(function (mutations) {
+					Objs.iter(mutations, function (mutation) {
+						for (var i = 0; i < mutation.addedNodes.length; ++i)
+							self._nodeInserted(mutation.addedNodes[i]);
+					});
+				});
+				this._observer.observe(this._options.root || this._options.parent || document.body, {
+					childList: true,
+					subtree: !!this._options.parent
+				});
+			},
+			
+			destroy: function () {
+				this._observer.disconnect();
+				inherited.destroy.call(this);
+			}
+			
+		};
+	}, {
+		
+		supported: function (node) {
+			try {
+				return !!window.MutationObserver;
+			} catch (e) {
+				return false;
+			}
+		}
+		
+	});	
+});
+
+
+
+Scoped.define("module:DomMutation.DOMNodeInsertedNodeInsertObserver", [
+	"module:DomMutation.NodeInsertObserver",
+	"jquery:"
+], function (Observer, $, scoped) {
+	return Observer.extend({scoped: scoped}, function (inherited) {
+		return {
+			
+			constructor: function (options) {
+				inherited.constructor.call(this, options);
+				var self = this;
+				$(document).on("DOMNodeInserted." + this.cid(), function (event) {
+					self._nodeInserted(event.target);
+				});
+			},
+			
+			destroy: function () {
+				$(document).off("DOMNodeInserted." + this.cid());
+				inherited.destroy.call(this);
+			}
+			
+		};
+	}, {
+		
+		supported: function (node) {
+			return true;
+		}
+		
+	});	
+});
+
+
+Scoped.extend("module:DomMutation.NodeInsertObserver", [
+	"module:DomMutation.NodeInsertObserver",
+	"module:DomMutation.MutationObserverNodeInsertObserver",
+	"module:DomMutation.DOMNodeInsertedNodeInsertObserver"
+], function (Observer, MutationObserverNodeInsertObserver, DOMNodeInsertedNodeInsertObserver) {
+	Observer.register(MutationObserverNodeInsertObserver, 2);
+	Observer.register(DOMNodeInsertedNodeInsertObserver, 1);
+	return {};
 });
 
 /*
@@ -1136,7 +1381,7 @@ Scoped.define("module:Info", [
 		
 		isiOS: function () {
 			return this.__cached("isiOS", function (nav, ua) {
-				if (this.isInternetExplorer())
+				if (this.isInternetExplorer() || this.isIEMobile())
 					return false;
 				return ua.indexOf('iPhone') != -1 || ua.indexOf('iPod') != -1 || ua.indexOf('iPad') != -1;
 			});
@@ -1154,9 +1399,27 @@ Scoped.define("module:Info", [
 			});
 		},
 		
+		isLocalCordova: function () {
+			return this.__cached("isLocalCordova", function () {
+				return this.isCordova() && document.location.href.indexOf("http") !== 0;
+			});
+		},
+
 		isChrome: function () {
 			return this.__cached("isChrome", function (nav, ua) {
 				return (nav.window_chrome || ua.indexOf('CriOS') != -1) && !this.isOpera() && !this.isEdge();
+			});
+		},
+		
+		isChromium: function () {
+			return this.__cached("isChromium", function (nav, ua, ualc) {
+				return !this.isChrome() && this.isAndroid() && ualc.indexOf("linux") >= 0;
+			});
+		},
+		
+		isChromiumBased: function () {
+			return this.__cached("isChromiumBased", function () {
+				return this.isChrome() || this.isChromium();
 			});
 		},
 		
@@ -1218,10 +1481,16 @@ Scoped.define("module:Info", [
 		isInternetExplorer: function () {
 			return this.__cached("isInternetExplorer", function () {
 				//return navigator.appName == 'Microsoft Internet Explorer';
-				return this.internetExplorerVersion() !== null;
+				return !this.isIEMobile() && this.internetExplorerVersion() !== null;
 			});
 		},
 		
+		isIEMobile: function () {
+			return this.__cached("isIEMobile", function (nav, ua, ualc) {
+				return ualc.indexOf("iemobile") >= 0;
+			});
+		},
+
 		isFirefox: function () {
 			return this.__cached("isFirefox", function (nav, ua, ualc) {
 				return ualc.indexOf("firefox") != -1 || ualc.indexOf("fxios") != -1;
@@ -1230,13 +1499,13 @@ Scoped.define("module:Info", [
 		
 		isSafari: function () {
 			return this.__cached("isSafari", function (nav, ua, ualc) {
-				return !this.isChrome() && !this.isOpera() && !this.isEdge() && !this.isFirefox() && ualc.indexOf("safari") != -1;
+				return !this.isChrome() && !this.isOpera() && !this.isEdge() && !this.isFirefox() && ualc.indexOf("safari") != -1 && !this.isAndroid();
 			});
 		},
 		
 		isWindows: function () {
 			return this.__cached("isWindows", function (nav) {
-				return nav.appVersion.toLowerCase().indexOf("win") != -1;
+				return nav.appVersion.toLowerCase().indexOf("win") != -1 && !this.isWindowsPhone();
 			});
 		},
 		
@@ -1397,6 +1666,9 @@ Scoped.define("module:Info", [
 		    chrome: {
 		    	format: "Chrome",
 		    	check: function () { return this.isChrome(); }
+		    }, chromium: {
+		    	format: "Chromium",
+		    	check: function () { return this.isChromium(); }
 		    }, opera: {
 		    	format: "Opera",
 		    	check: function () { return this.isOpera(); }
@@ -1412,22 +1684,19 @@ Scoped.define("module:Info", [
 		    }, safari: {
 		    	format: "Safari",
 		    	check: function () { return this.isSafari(); }
-		    }, android: {
-		    	format: "Android",
-		    	check: function () { return this.isAndroid() && !this.isChrome() && !this.isFirefox(); }
 		    }, webos: {
 		    	format: "WebOS",
 		    	check: function () { return this.isWebOS(); }
-		    }, windowsphone: {
-		    	format: "Windows Phone",
-		    	check: function () { return this.isWindowsPhone(); }
 		    }, blackberry: {
 		    	format: "Blackberry",
 		    	check: function () { return this.isBlackberry(); }
 		    }, edge: {
 		    	format: "Edge",
 		    	check: function () { return this.isEdge(); }
-		    }
+		    }, iemobile: {
+		    	format: "IE Mobile",
+		    	check: function () { return this.isIEMobile(); }
+		    }		    
 		},
 		
 		getBrowser: function () {
@@ -1763,7 +2032,7 @@ Scoped.define("module:Upload.FormDataFileUploader", [
     "jquery:",
     "base:Objs"
 ], function (FileUploader, Info, $, Objs, scoped) {
-	var Cls = FileUploader.extend({scoped: scoped}, {
+	return FileUploader.extend({scoped: scoped}, {
 		
 		_upload: function () {
 			var self = this;
@@ -1811,10 +2080,6 @@ Scoped.define("module:Upload.FormDataFileUploader", [
 		}
 		
 	});	
-	
-	FileUploader.register(Cls, 2);
-	
-	return Cls;
 });
 
 
@@ -1826,7 +2091,7 @@ Scoped.define("module:Upload.FormIframeFileUploader", [
      "json:",
      "base:Objs"
 ], function (FileUploader, $, Uri, JSON, Objs, scoped) {
-	var Cls = FileUploader.extend({scoped: scoped}, {
+	return FileUploader.extend({scoped: scoped}, {
 		
 		_upload: function () {
 			var self = this;
@@ -1889,10 +2154,6 @@ Scoped.define("module:Upload.FormIframeFileUploader", [
 		}
 		
 	});	
-	
-	FileUploader.register(Cls, 1);
-	
-	return Cls;
 });
 
 
@@ -1904,7 +2165,7 @@ Scoped.define("module:Upload.ResumableFileUploader", [
     "base:Objs",
     "jquery:"
 ], function (FileUploader, ResumableJS, Async, Objs, $, scoped) {
-	var Cls = FileUploader.extend({scoped: scoped}, {
+	return FileUploader.extend({scoped: scoped}, {
 		
 		_upload: function () {
 			this._resumable = new ResumableJS(Objs.extend({
@@ -1968,10 +2229,6 @@ Scoped.define("module:Upload.ResumableFileUploader", [
 		}
 		
 	});	
-	
-	FileUploader.register(Cls, 3);
-	
-	return Cls;
 });
 
 
@@ -1979,7 +2236,7 @@ Scoped.define("module:Upload.ResumableFileUploader", [
 Scoped.define("module:Upload.CordovaFileUploader", [
      "module:Upload.FileUploader"
 ], function (FileUploader, scoped) {
-	var Cls = FileUploader.extend({scoped: scoped}, {
+	return FileUploader.extend({scoped: scoped}, {
  		
  		_upload: function () {
  			var self = this;
@@ -2014,10 +2271,26 @@ Scoped.define("module:Upload.CordovaFileUploader", [
  		}
  		
  	});	
- 	
- 	FileUploader.register(Cls, 4);
- 	
- 	return Cls;
- });
+});
 
+
+Scoped.extend("module:Upload.FileUploader", [
+	"module:Upload.FileUploader",
+	"module:Upload.FormDataFileUploader",
+	"module:Upload.FormIframeFileUploader",
+	"module:Upload.CordovaFileUploader"
+], function (FileUploader, FormDataFileUploader, FormIframeFileUploader, CordovaFileUploader) {
+	FileUploader.register(FormDataFileUploader, 2);
+	FileUploader.register(FormIframeFileUploader, 1);
+	FileUploader.register(CordovaFileUploader, 4);
+	return {};
+});
+
+Scoped.extend("module:Upload.FileUploader", [
+	"module:Upload.FileUploader",
+	"module:Upload.ResumableFileUploader"
+], function (FileUploader, ResumableFileUploader) {
+	FileUploader.register(ResumableFileUploader, 3);
+	return {};
+});
 }).call(Scoped);
